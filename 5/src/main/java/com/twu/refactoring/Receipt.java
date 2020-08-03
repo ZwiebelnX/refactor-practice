f49d7b7d09a56f804c6ba70f19bf3a52bb1a9159
Chen Sicong <sicong.chen@163.com>
2020-08-03 23:53:43 +0800

    [Chen Sicong] refactor: exercise-5

--- 5/src/main/java/com/twu/refactoring/Receipt.java
+++ 5/src/main/java/com/twu/refactoring/Receipt.java
@@ -1,6 +1,8 @@
 package com.twu.refactoring;
 
 public class Receipt {
+
+    // TODO refactor here ...
     private static final int FIXED_CHARGE = 50;
     private static final double PEAK_TIME_MULTIPLIER = 1.2;
     private static final double OFF_PEAK_MULTIPLIER = 1.0;
@@ -20,19 +22,22 @@ public class Receipt {
     public double getTotalCost() {
         double totalCost = 0;
 
-        // fixed charges
-        totalCost += FIXED_CHARGE;
-
         // taxi charges
         int totalKms = taxi.getTotalKms();
-        double peakTimeMultiple = taxi.isPeakTime() ? PEAK_TIME_MULTIPLIER : OFF_PEAK_MULTIPLIER;
+
+        int preDistance = Math.min(RATE_CHANGE_DISTANCE, totalKms);
+        int postDistance = Math.max(0, totalKms - RATE_CHANGE_DISTANCE);
         if(taxi.isAirConditioned()) {
-            totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_AC_RATE * peakTimeMultiple;
-            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_AC_RATE * peakTimeMultiple;
+            totalCost += preDistance * PRE_RATE_CHANGE_AC_RATE + postDistance * POST_RATE_CHANGE_AC_RATE;
         } else {
-            totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
-            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
+            totalCost += preDistance * PRE_RATE_CHANGE_NON_AC_RATE + postDistance * POST_RATE_CHANGE_NON_AC_RATE;
         }
+
+        double peakTimeMultiple = taxi.isPeakTime() ? PEAK_TIME_MULTIPLIER : OFF_PEAK_MULTIPLIER;
+        totalCost *= peakTimeMultiple;
+
+        // fixed charges
+        totalCost += FIXED_CHARGE;
 
         return totalCost * (1 + SALES_TAX_RATE);
     }
