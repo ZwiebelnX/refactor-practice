8ca8e8defce57780d7e8e2c8d1926247eab43aaa
Chen Sicong <sicong.chen@163.com>
2020-08-03 23:33:37 +0800

    [Chen Sicong] refactor: exercise-4

--- 4/src/main/java/com/twu/refactoring/NumberCruncher.java
+++ 4/src/main/java/com/twu/refactoring/NumberCruncher.java
@@ -8,34 +8,33 @@ public class NumberCruncher {
     }
 
     public int countEven() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) count++;
-        }
-        return count;
+        return count((number) -> number % 2 == 0);
     }
 
     public int countOdd() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 1) count++;
-        }
-        return count;
+        return count((number -> number % 2 == 1));
     }
 
     public int countPositive() {
+        return count(number -> number >= 0);
+    }
+
+    public int countNegative() {
+        return count(number -> number < 0);
+    }
+
+    private int count(Action action) {
         int count = 0;
         for (int number : numbers) {
-            if (number >= 0) count++;
+            if (action.action(number)) {
+                count++;
+            }
         }
         return count;
     }
 
-    public int countNegative() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number < 0) count++;
-        }
-        return count;
+    @FunctionalInterface
+    private interface Action{
+        boolean action(int number);
     }
 }
